0c004a40097ee1b3ac10d81bafc0b613fc6c4d68
Rodrigo Nairnei <ronairnei@gmail.com>
2022-06-03 09:20:33 -0300

    small theme corrections

--- lib/main.dart
+++ lib/main.dart
@@ -8,9 +8,14 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Wlabel(name: "WPositivo", iconUrl: 'https://cdn.pixabay.com/photo/2016/06/01/16/40/thumb-1429327_960_720.png',);
+    return Wlabel(
+      name: "WPositivo",
+      // theme: ThemeData.dark(),
+      baseColor: Colors.blue,
+      iconUrl:
+          'https://mir-s3-cdn-cf.behance.net/projects/404/524754134780765.Y3JvcCwxNDMyLDExMjAsMTAyNCww.png',
+    );
   }
 }
